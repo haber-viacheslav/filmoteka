b3c3b321ffe501a20069b4a5b383048536f96ea1
Vladislav-UZH <perebziak.vladislav@gmail.com>
2022-12-06 21:52:39 +0200

    add renderApi new methods

--- src/js/helpers/renderFuncApi.js
+++ src/js/helpers/renderFuncApi.js
@@ -2,19 +2,34 @@ import refsApiServ from './refsApiServ';
 const refs = refsApiServ;
 
 class RenderApi {
-  constructor() {}
-
-  createMarkup(data) {
-    return data
-      .map(
-        ({}) => `
-        
-
-    `
-      )
-      .join('');
+  constructor() {
+    this.markup = ``;
   }
-  renderMarkup(params) {}
+
+  createMarkup(data, markup) {
+    return data.map(({}) => markup).join('');
+  }
+  renderMarkup({
+    selector,
+    innerHtml = false,
+    data,
+    insAdHtmltype = 'beforeend',
+  }) {
+    const elem = document.querySelector(selector);
+    if (innerHtml) {
+      elem.innerHTML = '';
+    }
+    elem.insertAdjacentHTML(
+      insAdHtmltype,
+      this.createMarkup(data, this.markup)
+    );
+  }
+  get actualMarkup() {
+    return this.markup;
+  }
+  set actualMarkup(newMarkup) {
+    this.markup = newMarkup;
+  }
 }
 
-export {};
+export default RenderApi;
