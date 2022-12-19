2b01c0a82c7d346a9cdd2c1536d4bdff7117a0fb
Vladislav-UZH <perebziak.vladislav@gmail.com>
2022-12-19 15:28:35 +0200

    add auth

--- src/js/firebase/auth.js
+++ src/js/firebase/auth.js
@@ -1,29 +1,21 @@
-import { initializeApp } from 'firebase/app';
-const firebaseConfig = {
-  apiKey: 'AIzaSyD7a39CtgzeuIx1kkpFRZ7MvO1bdmoGCT4',
-  authDomain: 'filmoteka-c3a99.firebaseapp.com',
-  databaseURL:
-    'https://filmoteka-c3a99-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'filmoteka-c3a99',
-  storageBucket: 'filmoteka-c3a99.appspot.com',
-  messagingSenderId: '412361816258',
-  appId: '1:412361816258:web:1c806438765e9ded7b235b',
-  measurementId: 'G-SJPWP9Q9CH',
-};
+import { app } from './init';
 
+import { postUserIntoDatebase, getUserDataById } from './postUserIntoDb';
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
 import {
   getAuth,
   signInWithPopup,
   GoogleAuthProvider,
   signOut,
 } from 'firebase/auth';
-
+app;
 // // SIGN IN
 
 const signInLinck = document.querySelector('.menu__link-js');
 // console.log(signIn.href);
+// if ((window.location.href = 'user-page')) {
+//   signInLinck.removeEventListener('click', onSignIn);
+// }
 signInLinck.addEventListener('click', onSignIn);
 
 async function onSignIn() {
@@ -43,6 +35,9 @@ async function onSignIn() {
 
       const user = result.user;
       console.log(user);
+      postUserIntoDatebase(user);
+      getUserDataById(user.uid, 'userData');
+
       if (user) {
         const navMenu = document.querySelector('.menu__list');
         console.log(navMenu);
--- src/js/firebase/backend.js
+++ src/js/firebase/backend.js
@@ -1,35 +1,15 @@
+import { app } from './init';
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, onValue } from 'firebase/database';
+import { getDatabase, ref, set } from 'firebase/database';
 
+import { getUserDataById } from './postUserIntoDb';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyD7a39CtgzeuIx1kkpFRZ7MvO1bdmoGCT4',
-  authDomain: 'filmoteka-c3a99.firebaseapp.com',
-  databaseURL:
-    'https://filmoteka-c3a99-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'filmoteka-c3a99',
-  storageBucket: 'filmoteka-c3a99.appspot.com',
-  messagingSenderId: '412361816258',
-  appId: '1:412361816258:web:1c806438765e9ded7b235b',
-  measurementId: 'G-SJPWP9Q9CH',
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+app;
 const db = getDatabase();
-
-import { getDatabase, ref, set } from 'firebase/database';
-import { getDatabase, ref, onValue } from 'firebase/database';
-
-const starCountRef = ref(db, 'users/');
-onValue(starCountRef, snapshot => {
-  const data = snapshot.val();
-  // console.log(data);
-});
 // function writeUserData(name, email, imageUrl) {
 //   set(ref(db, 'users/'), {
 //     username: name,
