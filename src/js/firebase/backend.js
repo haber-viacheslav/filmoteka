7d28877fc734a5be3af051d114e253aec28cd9d9
Haber Viacheslav <gaberslavik@gmail.com>
2022-12-20 11:26:27 +0200

    fix backend.js

--- src/js/firebase/backend.js
+++ src/js/firebase/backend.js
@@ -1,8 +1,8 @@
 import { app } from './initFirebase';
 import '../user-service/userServ';
 // Import the functions you need from the SDKs you need
-import { getDatabase, ref, onValue } from 'firebase/database';
-import { getDatabase, ref, set } from 'firebase/database';
+import { getDatabase, ref, set, onValue } from 'firebase/database';
+// import { getDatabase, ref, } from 'firebase/database'; // закоментировал, перезаписан
 import { getUserFilmsData } from '../user-service/userServ';
 
 // TODO: Add SDKs for Firebase products that you want to use
