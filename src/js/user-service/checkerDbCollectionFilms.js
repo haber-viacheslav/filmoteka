ad38d7fb367e171c719661af7bb54ce35b15baf6
Haber Viacheslav <gaberslavik@gmail.com>
2022-12-21 02:53:05 +0200

    fix user.js

--- src/js/user-service/checkerDbCollectionFilms.js
+++ src/js/user-service/checkerDbCollectionFilms.js
@@ -3,6 +3,6 @@ import { getFilmDataById, objData } from './postUserIntoDb';
 export function checkDbCollectionForMatches(userId, reference) {
   const obj = getFilmDataById(userId, reference);
 
-  console.log(objData);
+  // console.log(objData);
   // console.log('You already have this movie ');
 }
