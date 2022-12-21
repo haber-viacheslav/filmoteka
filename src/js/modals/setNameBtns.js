0590931975280827e01a37439b38ebb755c06e0a
Vladislav-UZH <perebziak.vladislav@gmail.com>
2022-12-21 13:02:58 +0200

    fix

---
+++ src/js/modals/setNameBtns.js
@@ -0,0 +1,17 @@
+import { refs } from '../helpers/refsApiServ';
+
+// function removeToQueue(e) {
+//   refs.addToQueue.textContent = 'DELETE FROM QUEUE';
+//   refs.addToQueue.classList.add('film-modal__btn-queue-active');
+//   // if (!addToQueue.textContent === 'ADD FROM QUEUE') {
+//   //   addToQueue.textContent = 'ADD FROM QUEUE'
+//   // }
+// }
+
+// function removeToWatch(e) {
+//   refs.addToWatch.textContent = 'DELETE FROM WATCHED';
+//   refs.addToWatch.classList.add('film-modal__btn-watched-active');
+//   // if (addToWatch.textContent === 'DELETE FROM WATCHED') {
+//   //   addToWatch.textContent = 'ADD FROM WATCHED'
+//   // }
+// }
